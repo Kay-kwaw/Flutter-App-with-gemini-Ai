3e5f6a2fc8c7648c407d938ac80b5f76e258ded1
kwaw kumi miezah <k.kumimiezah@gmail.com>
2024-05-23 11:27:15 +0000

    update

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,108 +1,169 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_ai/utilities/constant.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
-
+import 'package:flutter_gemini/flutter_gemini.dart' hide Content;
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:intl/intl.dart';
 
 final gemini = Gemini.instance;
-
+TextEditingController _controller = TextEditingController();
 
 class ChatScreen extends StatefulWidget {
-  static const String id = '/chat';
   const ChatScreen({super.key});
-
   @override
   State<ChatScreen> createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _userInput = TextEditingController();
+  static const apiKey = "<Replace You Gemini API Key Here>";
+
+  final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
+
+  final List<Message> _messages = [];
+
+  Future<void> sendMessage() async {
+    final message = _userInput.text;
+    setState(() {
+      _messages.add(Message(isUser: true, message: message, date: DateTime.now()));
+    });
+    final content = [Content.text(message)];
+    final response = await model.generateContent(content);
+    setState(() {
+      _messages.add(Message(isUser: false, message: response.text ?? "", date: DateTime.now()));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(
-        leading: Builder(
-          builder: (context) {
-            return Container(
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.black,
-              ),
-              child: IconButton(
-                icon: const Icon(Icons.menu, color: AppColors.textColor),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-              ),
-            );
-          }
-        ),
-        title: const Text('Gemini Ai Chatbot', style: TextStyle(color: AppColors.textColor)),
-        backgroundColor: Colors.black,
-        elevation: 2.0,
+        title: const Text('Chat Screen'),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back),
+        )
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: AppColors.primary,
-              ),
-              child: Text(
-                'Gemini Ai Chatbot',
-                style: TextStyle(
-                  color: AppColors.textColor,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.8), BlendMode.dstATop),
+            image: const NetworkImage(
+              'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEigDbiBM6I5Fx1Jbz-hj_mqL_KtAPlv9UsQwpthZIfFLjL-hvCmst09I-RbQsbVt5Z0QzYI_Xj1l8vkS8JrP6eUlgK89GJzbb_P-BwLhVP13PalBm8ga1hbW5pVx8bswNWCjqZj2XxTFvwQ__u4ytDKvfFi5I2W9MDtH3wFXxww19EVYkN8IzIDJLh_aw/s1920/space-soldier-ai-wallpaper-4k.webp'),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.all(8.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              child: ListView.builder(
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  final message = _messages[index];
+                  return Messages(
+                    isUser: message.isUser,
+                    message: message.message,
+                    date: DateFormat('HH:mm').format(message.date),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
                 children: [
-                  Center(
-                    child: Text('How can I help you?', style:TextStyles.heading4),
+                  Expanded(
+                    flex: 15,
+                    child: TextFormField(
+                      style: const TextStyle(color: Colors.white),
+                      controller: _userInput,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        label: const Text('Enter Your Message'),
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    padding: const EdgeInsets.all(12),
+                    iconSize: 30,
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black),
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                      shape: MaterialStateProperty.all(const CircleBorder()),
+                    ),
+                    onPressed: () {
+                      sendMessage();
+                    },
+                    icon: const Icon(Icons.send),
                   ),
                 ],
               ),
             ),
-            Padding(padding: EdgeInsets.only(bottom: 30.0, left: 10, right: 10),
-            child: Textfield(),
-            ),
           ],
-
         ),
-        ),
+      ),
     );
   }
 }
 
-class Textfield extends StatelessWidget {
-  const Textfield({
+class Message {
+  final bool isUser;
+  final String message;
+  final DateTime date;
+  Message({required this.isUser, required this.message, required this.date});
+}
+
+class Messages extends StatelessWidget {
+  final bool isUser;
+  final String message;
+  final String date;
+  const Messages({
     super.key,
+    required this.isUser,
+    required this.message,
+    required this.date,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: 'Type a message',
-        hintStyle: const TextStyle(color: AppColors.accentColor2),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0),
-          borderSide: BorderSide.none,
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.symmetric(vertical: 15).copyWith(
+        left: isUser ? 100 : 10,
+        right: isUser ? 10 : 100,
+      ),
+      decoration: BoxDecoration(
+        color: isUser ? Colors.blueAccent : Colors.grey.shade400,
+        borderRadius: BorderRadius.only(
+          topLeft: const Radius.circular(10),
+          bottomLeft: isUser ? const Radius.circular(10) : Radius.zero,
+          topRight: const Radius.circular(10),
+          bottomRight: isUser ? Radius.zero : const Radius.circular(10),
         ),
-        filled: true,
-        fillColor: const Color.fromARGB(31, 39, 39, 39),
-        contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            message,
+            style: TextStyle(
+              fontSize: 16,
+              color: isUser ? Colors.white : Colors.black,
+            ),
+          ),
+          Text(
+            date,
+            style: TextStyle(
+              fontSize: 10,
+              color: isUser ? Colors.white : Colors.black,
+            ),
+          ),
+        ],
       ),
     );
   }
